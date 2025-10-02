1fe59deaeace5d6ce7af6339c1b68333bdc07ec0
raimam29 <raima.mukherjee@amagi.com>
2025-10-02 15:37:23 +0530

    Implemented full auth, tab navigation

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,12 +1,65 @@
-import {View, Text, Button} from 'react-native'
-import React from 'react'
-import {router} from "expo-router";
+import {View, Text, Button, Alert} from 'react-native'
+import React, {useState} from 'react'
+import {Link, router} from "expo-router";
+import CustomInput from "@/components/CustomInput";
+import CustomButton from "@/components/CustomButton";
+import {createUser} from "@/lib/appwrite";
 
 const SignUp = () => {
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [form, setForm] = useState({name: '', email: "", password: ""});
+
+    const submit = async () => {
+        if (!form.name || !form.email || !form.password) return Alert.alert('Error',"Please enter a valid email address & password");
+
+        setIsSubmitting(true);
+
+        try {
+            await createUser({
+                email: form.email,
+                password: form.password,
+                name: form.name,
+            })
+            Alert.alert('Success', 'User signed up successfully');
+            router.replace('/'); //redirect to homepage
+        } catch (error: any) {
+            Alert.alert('Error', error.message);
+        } finally {
+            setIsSubmitting(false);
+        }
+    }
     return (
-        <View>
-            <Text>SignUp</Text>
-            <Button title="Sign Up" onPress={() => router.push('/sign-in')}/>
+        <View className="gap-10 bg-white rounded-lg p-5 mt-5">
+            <CustomInput
+                placeholder="Enter your full name"
+                value={form.name}
+                onChangeText={(text)=> setForm((prev)=> ({...prev, name: text}))}
+                label="Full Name"
+            />
+            <CustomInput
+                placeholder="Enter your email"
+                value={form.email}
+                onChangeText={(text)=> setForm((prev)=> ({...prev, email: text}))}
+                label="Email"
+                keyboardType="email-address"
+            />
+            <CustomInput
+                placeholder="Enter your password"
+                value={form.password}
+                onChangeText={(text)=> setForm((prev)=> ({...prev, password: text}))}
+                label="Password"
+                secureTextEntry={true}
+            />
+            <CustomButton
+                title="Sign Up"
+                isLoading={isSubmitting}
+                onPress={submit}
+            />
+
+            <View className="flex justify-center mt-5 flex-row gap-2">
+                <Text className="base-regular text-gray-100">Already have an account?</Text>
+                <Link href="/sign-in" className="base-bold text-primary">Sign In</Link>
+            </View>
         </View>
     )
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,17 @@
 import "../globals.css"
 import {SafeAreaView} from "react-native-safe-area-context";
-import {FlatList, Image, Pressable, ScrollView, Text, TouchableOpacity, View} from "react-native";
+import {Button, FlatList, Image, Pressable, ScrollView, Text, TouchableOpacity, View} from "react-native";
 import {images, offers} from "@/constants";
 import {Fragment} from "react";
 import cn from "clsx"
 import CartButton from "@/components/CartButton";
+import * as Sentry from "@sentry/react-native";
+import useAuthStore from "@/store/auth.store";
 
 export default function Index() {
+    const { user } = useAuthStore();
+
+    // console.log("USER: ", JSON.stringify(user, null, 2));
     return (
         <SafeAreaView className="flex-1 bg-white">
                 {/*Don't put flatlist inside scrollview*/}
@@ -56,6 +61,9 @@ export default function Index() {
                             <CartButton />
                         </View>
                     )}
+                    // ListFooterComponent={()=>(
+                    //     <Button title='Try!' onPress={ () => { Sentry.captureException(new Error('First error')) }}/>
+                    // )}
                 />
         </SafeAreaView>
     );
